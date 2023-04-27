66c1f13e79cba90c558fe3f3f12ecc5efa44f2b6
yuktnk <yuki0wl@gmail.com>
2023-04-27 21:13:49 +0900

    HeroIconsでsvgアイコンを用意

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { CartIcon } from "../Heroicons";
 
 const Navbar = () => {
   const { amount } = useSelector((store) => store.cart);
@@ -9,7 +10,7 @@ const Navbar = () => {
       <div className="nav-center">
         <h3>Redux Shopping</h3>
         <div className="nav-container">
-          ロゴ
+          <CartIcon />
           <div className="amount-container">
             <p className="total-amount">{amount}</p>
           </div>
